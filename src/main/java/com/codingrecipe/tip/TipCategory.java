14817c7def158eb52d1b410092cb11c4b0424437
KoHyeonJi <shinekhj1@gmail.com>
2025-07-25 13:20:32 +0900

    팁 카테고리 수정

--- src/main/java/com/codingrecipe/tip/TipCategory.java
+++ src/main/java/com/codingrecipe/tip/TipCategory.java
@@ -8,14 +8,10 @@ public enum TipCategory {
     LANGUAGE, // 언어
     LAW, // 법률
     MONEY, // 금융
-    FOOD, // 음식
     TECH, // 기술
     TRAVEL, // 여행
-    SHOPPING, // 쇼핑
     ETIQUETTE, // 예절
-    SUPPORT, // 지원
-    EMERGENCY, // 응급
-    WEATHER, // 날씨
+    SUPPORT, // 응급상황 및 지원
     K_CULTURE, // K-문화
     ETC // 기타
 }
