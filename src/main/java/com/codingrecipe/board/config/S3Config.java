bada6bd2661173bfac0b8d8da1d09b4f786f5e97
KoHyeonJi <shinekhj1@gmail.com>
2025-08-02 20:14:29 +0900

    Fix

--- src/main/java/com/codingrecipe/board/config/S3Config.java
+++ src/main/java/com/codingrecipe/board/config/S3Config.java
@@ -14,12 +14,6 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
     @Value("${cloud.aws.region.static}")
     private String region;
 
@@ -27,7 +21,6 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 
@@ -35,7 +28,6 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
