eec905a237a4b334efb7b60fbeadc783b87054c4
KoHyeonJi <shinekhj1@gmail.com>
2025-08-08 19:25:24 +0900

    Fix: redis 관련

--- src/main/java/com/codingrecipe/board/config/RedisConfig.java
+++ src/main/java/com/codingrecipe/board/config/RedisConfig.java
@@ -1,8 +1,12 @@
 package com.codingrecipe.board.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -13,11 +17,33 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.ssl:false}") // 기본은 false, 필요시 true
+    private boolean useSsl;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost.trim()); // 혹시 모를 공백 제거
+        config.setPort(redisPort);
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfig = LettuceClientConfiguration.builder();
+        if (useSsl) {
+            clientConfig.useSsl();
+        }
+
+        return new LettuceConnectionFactory(config, clientConfig.build());
+    }
+
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        // key, value 직렬화 설정 (문자열)
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
