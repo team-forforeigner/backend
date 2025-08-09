84d50b6786547860319768da7853bcbf2cea144f
KoHyeonJi <shinekhj1@gmail.com>
2025-08-09 22:15:49 +0900

    Fix

--- src/main/java/com/codingrecipe/board/config/RedisConfig.java
+++ src/main/java/com/codingrecipe/board/config/RedisConfig.java
@@ -1,18 +1,15 @@
 package com.codingrecipe.board.config;
 
-import io.lettuce.core.SocketOptions;
-import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -22,6 +19,7 @@ import java.util.List;
  * 설명 : Redis 설정 클래스입니다.
  */
 
+@Slf4j
 @Configuration
 public class RedisConfig {
 
@@ -36,6 +34,14 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.ssl:false}")
     private boolean useSsl;
+
+    @PostConstruct
+    public void logRedisProperties() {
+        log.info("Redis Cluster Nodes: {}", clusterNodes);
+        log.info("Redis Password set: {}", (password.isEmpty() ? "No" : "Yes"));
+        log.info("Redis Timeout (ms): {}", timeoutMillis);
+        log.info("Redis SSL Enabled: {}", useSsl);
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
