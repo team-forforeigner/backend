29d15519a9963ce8284b6ef0e505b033fd5fccdf
KoHyeonJi <shinekhj1@gmail.com>
2025-08-24 22:57:47 +0900

    Fix

--- src/main/java/com/codingrecipe/board/config/SecurityConfig.java
+++ src/main/java/com/codingrecipe/board/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                 "http://forforeigner.com.s3-website.ap-northeast-2.amazonaws.com",
                 "https://forforeigner.site",
                 "https://ai.navoodiai.site",
+                "https://api.forforeigner.site",
 
                 // 변경된 서버 IP 주소
                 "http://3.39.22.172"
